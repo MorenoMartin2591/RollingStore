93dea75dfe682fcd9ba367f10f45234d802e8ecc
Martin Moreno <tincho.m9111@gmail.com>
2023-03-04 12:47:48 -0300

    DOM+catalogoHTML

--- JS/dom.js
+++ JS/dom.js
@@ -89,22 +89,46 @@ const productos = [
   },
 ];
 
-const items = JSON.parse("items") || 
-
 /*Capturamos el elemento de la tabla*/
+const cargGrup = document.querySelector(".card-group");
 const tbody = document.querySelector("tbody");
 
 /*Recorro el array y genero un documento a insertar en el html*/
 productos.forEach((producto) => {
   tbody.innerHTML += `
-    <tr>
+  <tr>
     <th class="text-center">${producto.id}</th>
     <td class="text-center">${producto.tipo}</td>
     <td class="text-center">${producto.marca}</td>
     <td class="text-center">${producto.modelo}</td>
-    <td class="text-center">$ ${producto.precio}</td>    
+    <td class="text-center">${producto.precio}</td>
     <td><button class="button-editar btm btn-sm btn-warning">Editar</button></td>
-    <td><button class="button-borrar btm btn-sm btn-danger">Borra</button></td>
+    <td><button class="button-borrar btm btn-sm btn-danger onclick="quitarProductos(${productos.id})">Borra</button></td>
   </tr>
+    `;
+});
+
+/*Recorro el array y genero un documento a insertar en el html*/
+productos.forEach((producto) => {
+  cargGrup.innerHTML += `
+  <div class="col-3 m-1"> 
+  <img src="../Multimedia/img-section/Celulares/Motorola/moto E32.webp" class="card-img-top border-bottom"
+      alt="...">
+  <div class="card-body">
+      <h5 class="card-title text-center">${producto.marca} ${producto.modelo}</h5>
+  </div>
+
+  <div class="card-body d-flex justify-content-evenly">
+      <div class="card-body-preciomt text-primary">
+          <h5 class="card-title text-center">PRECIO MT</h5>
+          <h5 class="card-title text-center">${producto.precio}</h5>
+      </div>
+  </div>
+
+  <div class="card-body-button d-flex justify-content-evenly p-1">
+      <button type="button" class="btn btn-primary col-5">COMPRAR</button>
+      <button type="button" class="btn btn-secondary col-5">VER</button>
+  </div>
+</div>
     `;
 });
