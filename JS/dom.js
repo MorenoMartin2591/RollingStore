5fc04046771cfa55d5d296f99097694ba5911e65
Dario Alba <88122951+Darioalba@users.noreply.github.com>
2023-03-06 13:03:13 -0300

    se esta trabajado sobre admin de la pagina index

--- JS/dom.js
+++ JS/dom.js
@@ -36,77 +36,45 @@ const productos = [
     marca: "Samsung",
     modelo: "Galaxy A13",
     precio: 56.055,
-  },
-  {
-    id: 09,
-    tipo: "Celular",
-    marca: "Xiaomi",
-    modelo: "11T Pro",
-    precio: 299.999,
-  },
-  { id: 10, tipo: "Celular", marca: "Xiaomi", modelo: "11T", precio: 256.999 },
-  {
-    id: 11,
-    tipo: "Celular",
-    marca: "Xiaomi",
-    modelo: "Readmi Note 11",
-    precio: 162.999,
-  },
-  {
-    id: 12,
-    tipo: "Accesorios",
-    marca: "Xiaomi",
-    modelo: "Kit Cámara Xiaomi 4K",
-    precio: 145.999,
-  },
-  {
-    id: 13,
-    tipo: "Accesorios",
-    marca: "Xiaomi",
-    modelo: "Auriculares Inalambricos Xiaomi Buds 3T",
-    precio: 72.099,
-  },
-  {
-    id: 14,
-    tipo: "Accesorios",
-    marca: "Xiaomi",
-    modelo: "Cargador Inalámbrico 20W",
-    precio: 29.299,
-  },
-  {
-    id: 15,
-    tipo: "Accesorios",
-    marca: "Samsung",
-    modelo: "Auriculares Samsung Galaxy Pro",
-    precio: 31.999,
-  },
-  {
-    id: 16,
-    tipo: "Accesorios",
-    marca: "Samsung",
-    modelo: "Smart Watch Samsung Galaxy 40 MM",
-    precio: 29.119,
+
   },
 ];
 
 /*Capturamos el elemento de la tabla*/
 
-const tbody = document.querySelector("tbody");
+const tbody = document.querySelector(".card-group");
 
 /*Recorro el array y genero un documento a insertar en el html*/
 productos.forEach((producto) => {
   tbody.innerHTML += `
-  <tr>
-    <th class="text-center">${producto.id}</th>
-    <td class="text-center">${producto.tipo}</td>
-    <td class="text-center">${producto.marca}</td>
-    <td class="text-center">${producto.modelo}</td>
-    <td class="text-center">${producto.precio}</td>
-    <td><button class="button-editar btm btn-sm btn-warning" data-id="${producto.id}" onclick="editarProducto(${producto.id})">Editar</button></td>
-    <td><button class="button-borrar btm btn-sm btn-danger" data-id="${producto.id}" onclick="borrarProducto(${producto.id})">Borrar</button></td>
-  </tr>
+  <div class="card m-1 rounded-1">
+  <img src="./Multimedia/img-section/Celulares/Motorola/moto E20.jpg" class="card-img-top border-bottom"
+    alt="...">
+  <div class="card-body">
+    <h5 class="card-title text-center">Celular Libre ${producto.marca} ${producto.modelo}</h5>
+  </div>
+  <div class="card-body d-flex justify-content-evenly">
+    <div class="card-body-preciomt text-primary">
+      <h5 class="card-title text-center">PRECIO MT</h5>
+      <h5 class="card-title text-center">${producto.precio}</h5>
+    </div>
+    <div class="card-body-preciolista border-start">
+      <h6 class="card-title text-center ms-3">PRECIO LISTA</h6>
+      <h6 class="card-title text-center ms-3">$35.999</6>
+    </div>
+  </div>
+  <div class="card-body-cuotas">
+    <h6 class="card-title text-center">6 cuotas sin interes de $5.999,83</h6>
+  </div>
+  <div class="card-body-button d-flex justify-content-evenly p-1">
+    <button class="button-editar btm btn-sm btn-warning" data-id="${producto.id}" onclick="editarProducto(${producto.id})">Editar</button></td>
+    <button class="button-borrar btm btn-sm btn-danger" data-id="${producto.id}" onclick="borrarProducto(${producto.id})">Borrar</button></td>
+  </div>
+</div>
     `;
 });
+
+//funcion para borrar producto
 
 function borrarProducto(id) {
   const index = productos.findIndex((producto) => producto.id == id);
@@ -114,18 +82,35 @@ function borrarProducto(id) {
   tbody.innerHTML = "";
   productos.forEach((producto) => {
     tbody.innerHTML += `
-      <tr>
-        <th class="text-center">${producto.id}</th>
-        <td class="text-center">${producto.tipo}</td>
-        <td class="text-center">${producto.marca}</td>
-        <td class="text-center">${producto.modelo}</td>
-        <td class="text-center">${producto.precio}</td>
-        <td><button class="button-editar btm btn-sm btn-warning" data-id="${producto.id}" onclick="editarProducto(${producto.id})">Editar</button></td>
-        <td><button class="button-borrar btm btn-sm btn-danger" data-id="${producto.id}" onclick="borrarProducto(${producto.id})">Borrar</button></td>
-      </tr>
+    <div class="card m-1 rounded-1">
+    <img src="./Multimedia/img-section/Celulares/Motorola/moto E20.jpg" class="card-img-top border-bottom"
+      alt="...">
+    <div class="card-body">
+      <h5 class="card-title text-center">Celular Libre ${producto.marca} ${producto.modelo}</h5>
+    </div>
+    <div class="card-body d-flex justify-content-evenly">
+      <div class="card-body-preciomt text-primary">
+        <h5 class="card-title text-center">PRECIO MT</h5>
+        <h5 class="card-title text-center">${producto.precio}</h5>
+      </div>
+      <div class="card-body-preciolista border-start">
+        <h6 class="card-title text-center ms-3">PRECIO LISTA</h6>
+        <h6 class="card-title text-center ms-3">$35.999</6>
+      </div>
+    </div>
+    <div class="card-body-cuotas">
+      <h6 class="card-title text-center">6 cuotas sin interes de $5.999,83</h6>
+    </div>
+    <div class="card-body-button d-flex justify-content-evenly p-1">
+      <button class="button-editar btm btn-sm btn-warning" data-id="${producto.id}" onclick="editarProducto(${producto.id})">Editar</button></td>
+      <button class="button-borrar btm btn-sm btn-danger" data-id="${producto.id}" onclick="borrarProducto(${producto.id})">Borrar</button></td>
+    </div>
+  </div>
     `;
   });
 }
+
+//funcion para editar producto
 
 function editarProducto(id) {
   const index = productos.findIndex((producto) => producto.id == id);
@@ -155,19 +140,36 @@ function editarProducto(id) {
     tbody.innerHTML = "";
     productos.forEach((producto) => {
       tbody.innerHTML += `
-      <tr>
-        <th class="text-center">${producto.id}</th>
-        <td class="text-center">${producto.tipo}</td>
-        <td class="text-center">${producto.marca}</td>
-        <td class="text-center">${producto.modelo}</td>
-        <td class="text-center">${producto.precio}</td>
-        <td><button class="button-editar btm btn-sm btn-warning" data-id="${producto.id}" onclick="editarProducto(${producto.id})">Editar</button></td>
-        <td><button class="button-borrar btm btn-sm btn-danger" data-id="${producto.id}" onclick="borrarProducto(${producto.id})">Borrar</button></td>
-      </tr>
-      `;
+    <div class="card m-1 rounded-1">
+    <img src="./Multimedia/img-section/Celulares/Motorola/moto E20.jpg" class="card-img-top border-bottom"
+      alt="...">
+    <div class="card-body">
+      <h5 class="card-title text-center">Celular Libre ${producto.marca} ${producto.modelo}</h5>
+    </div>
+    <div class="card-body d-flex justify-content-evenly">
+      <div class="card-body-preciomt text-primary">
+        <h5 class="card-title text-center">PRECIO MT</h5>
+        <h5 class="card-title text-center">${producto.precio}</h5>
+      </div>
+      <div class="card-body-preciolista border-start">
+        <h6 class="card-title text-center ms-3">PRECIO LISTA</h6>
+        <h6 class="card-title text-center ms-3">$35.999</6>
+      </div>
+    </div>
+    <div class="card-body-cuotas">
+      <h6 class="card-title text-center">6 cuotas sin interes de $5.999,83</h6>
+    </div>
+    <div class="card-body-button d-flex justify-content-evenly p-1">
+      <button class="button-editar btm btn-sm btn-warning" data-id="${producto.id}" onclick="editarProducto(${producto.id})">Editar</button></td>
+      <button class="button-borrar btm btn-sm btn-danger" data-id="${producto.id}" onclick="borrarProducto(${producto.id})">Borrar</button></td>
+    </div>
+  </div>
+    `;
     });
   }
 }
+
+//funcion para agregar prodo
 
 const buttonAgregar = document.getElementById("button-agregar");
 
@@ -179,15 +181,33 @@ buttonAgregar.addEventListener("click", () => {
   const id = productos.length + 1;
   const nuevoProducto = { id, tipo, marca, modelo, precio };
   productos.push(nuevoProducto);
-  tbody.innerHTML += `
-    <tr>
-      <th class="text-center">${id}</th>
-      <td class="text-center">${tipo}</td>
-      <td class="text-center">${marca}</td>
-      <td class="text-center">${modelo}</td>
-      <td class="text-center">${precio}</td>
-      <td><button class="button-editar btm btn-sm btn-warning">Editar</button></td>
-      <td><button class="button-borrar btm btn-sm btn-danger" data-id="${id}">Borrar</button></td>
-    </tr>
-  `;
-});
+
+  tbody.innerHTML = "";
+    productos.forEach((producto) => {
+      tbody.innerHTML += `
+  <div class="card m-1 rounded-1">
+  <img src="./Multimedia/img-section/Celulares/Motorola/moto E20.jpg" class="card-img-top border-bottom"
+    alt="...">
+  <div class="card-body">
+    <h5 class="card-title text-center">Celular Libre ${producto.marca} ${producto.modelo}</h5>
+  </div>
+  <div class="card-body d-flex justify-content-evenly">
+    <div class="card-body-preciomt text-primary">
+      <h5 class="card-title text-center">PRECIO MT</h5>
+      <h5 class="card-title text-center">${producto.precio}</h5>
+    </div>
+    <div class="card-body-preciolista border-start">
+      <h6 class="card-title text-center ms-3">PRECIO LISTA</h6>
+      <h6 class="card-title text-center ms-3">$35.999</6>
+    </div>
+  </div>
+  <div class="card-body-cuotas">
+    <h6 class="card-title text-center">6 cuotas sin interes de $5.999,83</h6>
+  </div>
+  <div class="card-body-button d-flex justify-content-evenly p-1">
+    <button class="button-editar btm btn-sm btn-warning" data-id="${producto.id}" onclick="editarProducto(${producto.id})">Editar</button></td>
+    <button class="button-borrar btm btn-sm btn-danger" data-id="${producto.id}" onclick="borrarProducto(${producto.id})">Borrar</button></td>
+  </div>
+</div>
+  // `})
+})
