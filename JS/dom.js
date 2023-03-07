e0e654427b242701a0a0c34f880a77c57c34499c
Martin Moreno <tincho.m9111@gmail.com>
2023-03-07 17:39:15 -0300

    se trabaja sobre admin y registro

--- JS/dom.js
+++ JS/dom.js
@@ -1,14 +1,14 @@
 const productos = [
   { id: 01, tipo: "Celular", marca: "Motorola", modelo: "E32", precio: 46.045 },
   { id: 02, tipo: "Celular", marca: "Motorola", modelo: "E22", precio: 46.045 },
-  // {
-  //   id: 03,
-  //   tipo: "Celular",
-  //   marca: "Motorola",
-  //   modelo: "E22i",
-  //   precio: 42.542,
-  // },
-  // { id: 04, tipo: "Celular", marca: "Motorola", modelo: "EG32", precio: 70.57 },
+  {
+    id: 03,
+    tipo: "Celular",
+    marca: "Motorola",
+    modelo: "E22i",
+    precio: 42.542,
+  },
+  { id: 04, tipo: "Celular", marca: "Motorola", modelo: "EG32", precio: 70.57 },
   // {
   //   id: 05,
   //   tipo: "Celular",
@@ -109,21 +109,35 @@ leerProductoslocal();
 
 /*Capturamos el elemento de la tabla*/
 
-const tbody = document.querySelector("tbody");
+const tbody = document.querySelector(".card-group");
 
 /*Recorro el array y genero un documento a insertar en el html*/
 productos.forEach((producto) => {
   tbody.innerHTML += `
-  <tr>
-    <th class="text-center">${producto.id}</th>
-    <td class="text-center">${producto.tipo}</td>
-    <td class="text-center">${producto.marca}</td>
-    <td class="text-center">${producto.modelo}</td>
-    <td class="text-center">${producto.precio}</td>
-    <td><button class="button-editar btm btn-sm btn-warning" data-id="${producto.id}" onclick="editarProducto(${producto.id})">Editar</button></td>
-    <td><button class="button-borrar btm btn-sm btn-danger" data-id="${producto.id}" onclick="borrarProducto(${producto.id})">Borrar</button></td>
-  </tr>
-    `;
+  <div class="card m-1 rounded-1">
+  <img src="./Multimedia/img-section/Celulares/Motorola/moto E20.jpg" class="card-img-top border-bottom"
+    alt="...">
+  <div class="card-body">
+    <h5 class="card-title text-center">Celular Libre ${producto.marca} ${producto.modelo}</h5>
+  </div>
+  <div class="card-body d-flex justify-content-evenly">
+    <div class="card-body-preciomt text-primary">
+      <h5 class="card-title text-center">PRECIO MT</h5>
+      <h5 class="card-title text-center">${producto.precio}</h5>
+    </div>
+    <div class="card-body-preciolista border-start">
+      <h6 class="card-title text-center ms-3">PRECIO LISTA</h6>
+      <h6 class="card-title text-center ms-3">$35.999</6>
+    </div>
+  </div>
+  <div class="card-body-cuotas">
+    <h6 class="card-title text-center">6 cuotas sin interes de $5.999,83</h6>
+  </div>
+  <div class="card-body-button d-flex justify-content-evenly p-1">
+    <button type="button" class="btn btn-primary col-5">COMPRAR</button>
+    <button type="button" class="btn btn-secondary col-5">VER</button>
+  </div>
+</div>`;
 });
 
 function borrarProducto(id) {
@@ -132,15 +146,30 @@ function borrarProducto(id) {
   tbody.innerHTML = "";
   productos.forEach((producto) => {
     tbody.innerHTML += `
-      <tr>
-        <th class="text-center">${producto.id}</th>
-        <td class="text-center">${producto.tipo}</td>
-        <td class="text-center">${producto.marca}</td>
-        <td class="text-center">${producto.modelo}</td>
-        <td class="text-center">${producto.precio}</td>
-        <td><button class="button-editar btm btn-sm btn-warning" data-id="${producto.id}" onclick="editarProducto(${producto.id})">Editar</button></td>
-        <td><button class="button-borrar btm btn-sm btn-danger" data-id="${producto.id}" onclick="borrarProducto(${producto.id})">Borrar</button></td>
-      </tr>
+    <div class="card m-1 rounded-1">
+    <img src="./Multimedia/img-section/Celulares/Motorola/moto E20.jpg" class="card-img-top border-bottom"
+      alt="...">
+    <div class="card-body">
+      <h5 class="card-title text-center">Celular Libre ${producto.marca} ${producto.modelo}</h5>
+    </div>
+    <div class="card-body d-flex justify-content-evenly">
+      <div class="card-body-preciomt text-primary">
+        <h5 class="card-title text-center">PRECIO MT</h5>
+        <h5 class="card-title text-center">${producto.precio}</h5>
+      </div>
+      <div class="card-body-preciolista border-start">
+        <h6 class="card-title text-center ms-3">PRECIO LISTA</h6>
+        <h6 class="card-title text-center ms-3">$35.999</6>
+      </div>
+    </div>
+    <div class="card-body-cuotas">
+      <h6 class="card-title text-center">6 cuotas sin interes de $5.999,83</h6>
+    </div>
+    <div class="card-body-button d-flex justify-content-evenly p-1">
+      <button type="button" class="btn btn-primary col-5">COMPRAR</button>
+      <button type="button" class="btn btn-secondary col-5">VER</button>
+    </div>
+  </div>
     `;
   });
 }
@@ -173,15 +202,30 @@ function editarProducto(id) {
     tbody.innerHTML = "";
     productos.forEach((producto) => {
       tbody.innerHTML += `
-      <tr>
-        <th class="text-center">${producto.id}</th>
-        <td class="text-center">${producto.tipo}</td>
-        <td class="text-center">${producto.marca}</td>
-        <td class="text-center">${producto.modelo}</td>
-        <td class="text-center">${producto.precio}</td>
-        <td><button class="button-editar btm btn-sm btn-warning" data-id="${producto.id}" onclick="editarProducto(${producto.id})">Editar</button></td>
-        <td><button class="button-borrar btm btn-sm btn-danger" data-id="${producto.id}" onclick="borrarProducto(${producto.id})">Borrar</button></td>
-      </tr>
+      <div class="card m-1 rounded-1">
+      <img src="./Multimedia/img-section/Celulares/Motorola/moto E20.jpg" class="card-img-top border-bottom"
+        alt="...">
+      <div class="card-body">
+        <h5 class="card-title text-center">Celular Libre ${producto.marca} ${producto.modelo}</h5>
+      </div>
+      <div class="card-body d-flex justify-content-evenly">
+        <div class="card-body-preciomt text-primary">
+          <h5 class="card-title text-center">PRECIO MT</h5>
+          <h5 class="card-title text-center">${producto.precio}</h5>
+        </div>
+        <div class="card-body-preciolista border-start">
+          <h6 class="card-title text-center ms-3">PRECIO LISTA</h6>
+          <h6 class="card-title text-center ms-3">$35.999</6>
+        </div>
+      </div>
+      <div class="card-body-cuotas">
+        <h6 class="card-title text-center">6 cuotas sin interes de $5.999,83</h6>
+      </div>
+      <div class="card-body-button d-flex justify-content-evenly p-1">
+        <button type="button" class="btn btn-primary col-5">COMPRAR</button>
+        <button type="button" class="btn btn-secondary col-5">VER</button>
+      </div>
+    </div>
       `;
     });
   }
@@ -199,14 +243,29 @@ buttonAgregar.addEventListener("click", () => {
   productos.push(nuevoProducto);
   localStorage.setItem("productosLocal", JSON.stringify(productos));
   tbody.innerHTML += `
-    <tr>
-      <th class="text-center">${id}</th>
-      <td class="text-center">${tipo}</td>
-      <td class="text-center">${marca}</td>
-      <td class="text-center">${modelo}</td>
-      <td class="text-center">${precio}</td>
-      <td><button class="button-editar btm btn-sm btn-warning">Editar</button></td>
-      <td><button class="button-borrar btm btn-sm btn-danger" data-id="${id}">Borrar</button></td>
-    </tr>
+  <div class="card m-1 rounded-1">
+  <img src="./Multimedia/img-section/Celulares/Motorola/moto E20.jpg" class="card-img-top border-bottom"
+    alt="...">
+  <div class="card-body">
+    <h5 class="card-title text-center">Celular Libre ${producto.marca} ${producto.modelo}</h5>
+  </div>
+  <div class="card-body d-flex justify-content-evenly">
+    <div class="card-body-preciomt text-primary">
+      <h5 class="card-title text-center">PRECIO MT</h5>
+      <h5 class="card-title text-center">${producto.precio}</h5>
+    </div>
+    <div class="card-body-preciolista border-start">
+      <h6 class="card-title text-center ms-3">PRECIO LISTA</h6>
+      <h6 class="card-title text-center ms-3">$35.999</6>
+    </div>
+  </div>
+  <div class="card-body-cuotas">
+    <h6 class="card-title text-center">6 cuotas sin interes de $5.999,83</h6>
+  </div>
+  <div class="card-body-button d-flex justify-content-evenly p-1">
+    <button type="button" class="btn btn-primary col-5">COMPRAR</button>
+    <button type="button" class="btn btn-secondary col-5">VER</button>
+  </div>
+</div>
   `;
 });
